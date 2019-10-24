6572dfcc5fc03e82f2f0dacae327ffe7243e5dd8
lepamateusz <lepa.mateusz@gmail.com>
2019-10-24 21:10:41 +0200

    init script

---
+++ ImageSiteDownloader.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python3
+
+# ImageSiteDownloander.py - program that goes to a photo-sharing site like Flickr or Imgur, searches for a category of photos, and then downloads all the resulting images.
